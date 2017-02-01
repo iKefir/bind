be947a15cec70b5b74c8c7197ee2defc0a2072e2
iKefir <danshk@mail.ru>
2017-02-01 22:41:57 +0300

    first revision

---
+++ bind/bind.cpp
@@ -0,0 +1,33 @@
+#include "bind.hpp"
+#include <iostream>
+#include <functional>
+
+using std::cout;
+using std::endl;
+
+int make_arithmetics(int a, int b, int c, int d, int e) {
+    return a + b - c / d * e;
+}
+
+int main() {
+    auto my_bind_1 = bind(make_arithmetics, 1, 2, 3, 4, 5);
+    auto st_bind_1 = std::bind(make_arithmetics, 1, 2, 3, 4, 5);
+    
+    cout << my_bind_1() << " " << st_bind_1() << endl;
+    
+    auto my_bind_2 = bind(make_arithmetics, 1, 2, 3, _1, _2);
+    auto st_bind_2 = std::bind(make_arithmetics, 1, 2, 3, std::placeholders::_1, std::placeholders::_2);
+    
+    cout << my_bind_2(4, 5) << " " << st_bind_2(4, 5) << endl;
+    
+    auto my_bind_3 = bind(make_arithmetics, bind([](int a) {return -a;}, _1), 2, 3, _1, _2);
+    auto st_bind_3 = std::bind(make_arithmetics, std::bind([](int a) {return -a;}, std::placeholders::_1), 2, 3, std::placeholders::_1, std::placeholders::_2);
+    
+    cout << my_bind_3(4, 5) << " " << st_bind_3(4, 5) << endl;
+    
+    auto my_bind_4 = bind(my_bind_2, 1, 2);
+    auto st_bind_4 = std::bind(st_bind_2, 1, 2);
+    
+    cout << my_bind_4() << " " << st_bind_4() << endl;
+}
+
---
+++ bind/bind.hpp
@@ -0,0 +1,81 @@
+#ifndef bind_hpp
+#define bind_hpp
+
+#include <stdio.h>
+#include <utility>
+#include <tuple>
+
+template <size_t N>
+struct placeholder {};
+
+template <>
+struct placeholder<0>;
+
+placeholder<1> _1;
+placeholder<2> _2;
+placeholder<3> _3;
+placeholder<4> _4;
+placeholder<5> _5;
+placeholder<6> _6;
+placeholder<7> _7;
+placeholder<8> _8;
+placeholder<9> _9;
+placeholder<10> _10;
+
+template <typename Func, typename ...Args>
+struct bind_obj {
+    
+    bind_obj(Func&& f, Args&&... args) :
+    func(std::forward<Func>(f)), args(std::forward<Args>(args)...)
+    {}
+    
+    template <typename ... New_args>
+    auto operator() (New_args&& ...new_args) {
+        return call_f(typename get_seq<std::tuple_size<std::tuple<Args...>>::value>::type(), std::forward<New_args>(new_args)...);
+    }
+    
+private:
+    std::tuple<Args...> args;
+    Func func;
+    
+    template <size_t... sequence>
+    struct seq {};
+    
+    template <size_t first, size_t ...other>
+    struct get_seq {
+        typedef typename get_seq<first - 1, first - 1, other...>::type type;
+    };
+    
+    template <size_t ...other>
+    struct get_seq<0, other...> {
+        typedef seq<other...> type;
+    };
+    
+    template <typename That_arg, typename ...New_args>
+    auto&& get_arg(That_arg&& that_arg, New_args& ...new_args) {
+        return that_arg;
+    }
+    
+    template <size_t N, typename ...New_args>
+    auto&& get_arg(placeholder<N> plh, New_args& ...new_args) {
+        return std::get<N - 1>(std::forward_as_tuple(new_args...));
+    }
+    
+    template <typename Other_func, typename ...Other_args, typename ...New_args>
+    auto get_arg(bind_obj<Other_func, Other_args...> & other_bind, New_args& ...new_args) {
+        return other_bind(new_args...);
+    }
+    
+    template <size_t ...N, typename ...New_args>
+    auto call_f(const seq<N...>& sequence, New_args&& ...new_args) {
+        return func(get_arg(std::get<N>(args), new_args...)...);
+    }
+    
+};
+
+template <typename F, typename ...Args>
+bind_obj<F, Args...> bind(F&& f, Args&&... args) {
+    return bind_obj<F, Args...>(std::forward<F>(f), std::forward<Args>(args)...);
+}
+
+#endif /* bind_hpp */
